7fe297abda8560ba2f657ab5ab68225e3f8c0c0a
Raviteja Ranthu <61727389+ravitejarmkv@users.noreply.github.com>
2021-06-17 13:58:37 +0530

    Add files via upload

---
+++ Weather_Report.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+API_KEY = "API KEY"
+
+user = "yes"
+while user == "yes" or user == "y":
+    city = input("Enter city name: ")
+    URL = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    res = requests.get(URL)
+    json_data = json.loads(res.text)
+
+    if json_data["cod"] != "404":
+        weather = json_data["weather"][0]["description"]
+        temperature = json_data["main"]["temp"]
+        humidity = json_data["main"]["humidity"]
+        wind_speed = json_data["wind"]["speed"]
+        print(f"Weather:{weather}")
+        print(f"Temperature:{temperature} Celsius")
+        print(f"Humidity:{humidity}")
+        print(f"Wind Speed:{wind_speed}")
+    else:
+        print("City not found")
+    user = input("Search again? (yes/y): ")
+
+print("Program ended")
